312d39cc990af0d3ee6027b7b9a422ee72588ee0
clarkjiminca <clarkjiminca@gmail.com>
2024-01-08 00:37:54 +0800

    2024-01-08 00:37:54

--- main.py
+++ main.py
@@ -115,3 +115,4 @@ void ParserYD::notifyLogin(int errorNo, int maxOrderRef, bool isMonitor)
 
 void ParserYD::notifyFinishInit(void)
 {
+	if (m_sink) m_sink->handleParserLog(LL_INFO, "[ParserYD] All things ready");
