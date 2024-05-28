f9b27ae09e0600206a0b78c136d88f9c86a8082e
clarkjiminca <clarkjiminca@gmail.com>
2024-05-28 22:59:10 +0800

    2024-05-28 22:59:10

--- main.py
+++ main.py
@@ -160,3 +160,4 @@ void ParserYD::notifyMarketData(const YDMarketData *pDepthMarketData)
 
 	quote.upper_limit = pDepthMarketData->UpperLimitPrice;
 	quote.lower_limit = pDepthMarketData->LowerLimitPrice;
+
