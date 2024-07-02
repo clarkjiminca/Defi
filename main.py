78346f89f112c537ae8aa46e699536d9ff14ced6
clarkjiminca <clarkjiminca@gmail.com>
2024-07-03 00:57:07 +0800

    2024-07-03 00:57:07

--- main.py
+++ main.py
@@ -208,3 +208,4 @@ bool ParserYD::init(WTSVariant* config)
  //By Wesley @ 2022.01.05
 #include "../Share/fmtlib.h"
 template<typename... Args>
+inline void write_log(IParserSpi* sink, WTSLogLevel ll, const char* format, const Args&... args)
