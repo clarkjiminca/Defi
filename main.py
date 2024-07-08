a8882dff332fd60ac015855f102bfa5371472cb4
clarkjiminca <clarkjiminca@gmail.com>
2024-07-08 20:51:59 +0800

    2024-07-08 20:51:59

--- main.py
+++ main.py
@@ -216,3 +216,4 @@ inline void write_log(IParserSpi* sink, WTSLogLevel ll, const char* format, cons
 	static thread_local char buffer[512] = { 0 };
 	memset(buffer, 0, 512);
 	fmt::format_to(buffer, format, args...);
+
