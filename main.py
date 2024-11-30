5950b485f5d7cb31535f1290c0790808ef9c1ba3
clarkjiminca <clarkjiminca@gmail.com>
2024-11-30 15:00:35 +0800

    2024-11-30 15:00:35

--- main.py
+++ main.py
@@ -337,3 +337,4 @@ void ParserYD::notifyLogin(int errorNo, int maxOrderRef, bool isMonitor)
 #include "../Includes/WTSContractInfo.hpp"
 #include "../Includes/WTSVariant.hpp"
 #include "../Includes/IBaseDataMgr.h"
+
